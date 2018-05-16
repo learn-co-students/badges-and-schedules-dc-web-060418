ba6e4952691b6e5a39fdccb921b7eb0c6c00dab0
Susanna Kosonen <susannakosonen@USF-Gold-Wifi-226-41-79.laptops.usf.edu>
2018-05-16 16:30:14 -0400

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,28 @@
 # Write your code here.
+def badge_maker(name)
+    return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+    badges = []
+    array.each do |name|
+        badges.push(badge_maker(name))
+    end
+    badges
+end
+
+def assign_rooms(array)
+    array.map! do |name|
+        "Hello, #{name}! You'll be assigned to room #{array.index(name)+1}!"
+    end
+    array
+end
+
+def printer(attendees)
+    batch_badge_creator(attendees).each do |item|
+        puts item
+    end
+    assign_rooms(attendees).each do |item|
+        puts item
+    end
+end
