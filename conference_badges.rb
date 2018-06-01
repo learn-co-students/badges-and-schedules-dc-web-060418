c0c2420b57a4638287995ec70f9c5178d8f0071b
github username <github email address>
2018-06-01 04:42:48 -0400

    Badges and Schedules

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,42 @@
 # Write your code here.
+
+def badge_maker(name)
+    return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+    newArray = []
+
+    attendees.each do |name|
+        newArray.push("Hello, my name is #{name}.")
+    end
+
+    return newArray
+end
+
+def assign_rooms(attendees)
+    newArray = []
+    roomNumber = 1
+    
+    attendees.each do |name|
+        newArray.push("Hello, #{name}! You'll be assigned to room #{roomNumber}!")
+        roomNumber += 1
+    end
+
+    return newArray
+end
+
+def printer(attendees)
+
+    badgesArray = batch_badge_creator(attendees)
+    roomsArray = assign_rooms(attendees)
+
+    badgesArray.each do |badge|
+        puts badge
+    end
+
+    roomsArray.each do |room|
+        puts room
+    end
+
+end
